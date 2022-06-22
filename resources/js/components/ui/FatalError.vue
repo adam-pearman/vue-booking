63f0326565135b74d58e13eb312a7ef69b354240
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-06-22 19:42:02 +0100

    Create FatalError.vue

---
+++ resources/js/components/ui/FatalError.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-center text-2xl text-gray-500">
+        <BombIcon class="mx-auto py-10 w-1/12 fill-gray-500"/>
+        <h1>Uh oh! We've encountered a problem...</h1>
+    </div>
+</template>
+
+<script setup>
+import BombIcon from '../icons/BombIcon'
+</script>
