e73174e22b41cfd265acb8a4c78f779ce8ec3ac3
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-06-27 20:59:33 +0100

    Create ValidationErrors.vue

---
+++ resources/js/components/ui/ValidationErrors.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="text-red-600" v-for="error in errors">
+        {{ error }}
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    errors: {
+        type: Array,
+        required: true,
+    }
+})
+</script>
