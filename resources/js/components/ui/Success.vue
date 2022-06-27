7649fe0d394bac2e58c267cd99031ecd3a6ff83d
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-06-27 22:09:13 +0100

    Create Success.vue

---
+++ resources/js/components/ui/Success.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-center text-gray-500">
+        <ThumbIcon class="mx-auto py-10 w-1/12 fill-gray-500"/>
+        <h1 class="text-2xl">Success!</h1>
+        <h2 class="text-xl"><slot/></h2>
+    </div>
+</template>
+
+<script setup>
+import ThumbIcon from '../icons/ThumbIcon'
+</script>
