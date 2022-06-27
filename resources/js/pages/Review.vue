659c2c4625045853e5cd004b385d83fc7d46d6ca
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-06-27 22:09:20 +0100

    Update Review.vue

--- resources/js/pages/Review.vue
+++ resources/js/pages/Review.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="container mx-auto">
+        <div class="w-full" v-if="success">
+            <Success>
+                You have left a review!
+            </Success>
+        </div>
         <div class="w-full" v-if="error">
             <FatalError/>
         </div>
-        <div class="w-full flex" v-else>
+        <div class="w-full flex" v-if="!success && !error">
             <div class="flex flex-col"
                  :class="[{'md:w-4/12' : twoColumns},
                   {'hidden' : oneColumn}]">
@@ -36,7 +41,7 @@
                 <div v-else>
                     <form @submit.prevent="submit" class="grid grid-cols-1 place-content-center w-3/4 mx-auto">
                         <label class="mb-2">Select the star rating (1 is worst - 5 is best)</label>
-                        <StarRating v-model:rating="review.rating" :size="48" class="mb-6"/>
+                        <StarRating v-if="ratingCheck" v-model:rating="review.rating" :size="48" class="mb-6"/>
                         <label for="content" class="mb-2">Describe your experience with {{reservation.booking.title}}</label>
                         <textarea name="content"
                                   id="content"
@@ -70,6 +75,7 @@ import {useIsError} from "../utils/useMethods";
 import FatalError from "../components/ui/FatalError";
 import ValidationErrors from "../components/ui/ValidationErrors";
 import _ from "lodash";
+import Success from "../components/ui/Success";
 
 const route = useRoute();
 const existingReview = ref(null);
@@ -78,6 +84,7 @@ const loading = ref(false);
 const error = ref(false);
 const errors = ref(null);
 const sending = ref(false);
+const success = ref(false);
 
 const review = ref({
     id: null,
@@ -85,48 +92,39 @@ const review = ref({
     content: null
 })
 
-const alreadyReviewed = computed(() => {
-    return hasReview.value || !hasReservation.value;
-});
-const hasReview = computed(() => {
-    return existingReview.value !== null;
-});
-const hasReservation = computed(() => {
-    return reservation.value !== null;
-})
-const oneColumn = computed(() => {
-    return !loading.value && alreadyReviewed.value;
-})
-const twoColumns = computed(() => {
-    return loading.value || !alreadyReviewed.value;
-})
+const alreadyReviewed = computed(() => hasReview.value || !hasReservation.value);
+const hasReview = computed(() => existingReview.value !== null);
+const hasReservation = computed(() => reservation.value !== null);
+const oneColumn = computed(() => !loading.value && alreadyReviewed.value);
+const twoColumns = computed(() => loading.value || !alreadyReviewed.value);
+const ratingCheck = computed(() => review.value.rating > 0 && review.value.rating < 6);
 
-function checkReview() {
+async function checkReview() {
     review.value.id = route.params.id;
     loading.value = true;
-    axios.get(`/api/v1/reviews/${review.value.id}`)
-        .then(response => {
-            existingReview.value = response.data.data
-        })
-        .catch(err => {
-            if (useIsError(err, 404)) {
-                return axios.get(`/api/v1/reservation-by-review/${review.value.id}`)
-                    .then(response => {
-                        reservation.value = response.data.data;
-                    })
-                    .catch(err => {
-                        error.value = !useIsError(err, 404);
-                    });
+
+    try {
+        existingReview.value = (await axios.get(`/api/v1/reviews/${review.value.id}`)).data.data;
+    } catch (err) {
+        if (useIsError(err, 404)) {
+            try {
+                reservation.value = (await (axios.get(`/api/v1/reservation-by-review/${review.value.id}`))).data.data;
+            } catch (err) {
+                error.value = !useIsError(err, 404);
             }
-        })
-        .then(() => loading.value = false);
+        } else {
+            error.value = true;
+        }
+    }
+    loading.value = false;
 }
 
 function submit() {
     errors.value = null;
     sending.value = true;
+    review.value.rating = 6;
     axios.post(`/api/v1/reviews`, review.value)
-        .then(response => console.log(response))
+        .then(response => success.value = response.status === 201)
         .catch(err => {
             if (useIsError(err, 422)) {
                 const errs = err.response.data.errors;
@@ -138,7 +136,6 @@ function submit() {
             error.value = true;
         })
         .then(() => {
-            console.log(errors.value)
             return sending.value = false
         });
 }
