04577ed8f740ef4e0651e55ef8dd8b2449314cb8
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-06-27 22:09:16 +0100

    Update FatalError.vue

--- resources/js/components/ui/FatalError.vue
+++ resources/js/components/ui/FatalError.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="text-center text-2xl text-gray-500">
+    <div class="text-center text-gray-500">
         <BombIcon class="mx-auto py-10 w-1/12 fill-gray-500"/>
-        <h1>Uh oh! We've encountered a problem...</h1>
+        <h1 class="text-2xl">Boom!</h1>
+        <h2 class="text-xl">What did you do!?</h2>
     </div>
 </template>
 
