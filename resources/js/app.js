a320837709d9535bfb052068b594c6010b382f14
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-07-27 06:25:33 +0100

    install pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,11 @@
 require('./bootstrap');
 
 import {createApp} from 'vue';
-
+import {createPinia} from "pinia";
 import App from './App.vue';
 import router from './router';
 
 createApp(App)
     .use(router)
+    .use(createPinia())
     .mount('#app');
