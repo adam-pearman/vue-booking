ff2e6ec0856f0ca73abd707367944c895c143c2b
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-07-27 08:29:47 +0100

    Create PriceBreakdown.vue

---
+++ resources/js/components/booking/PriceBreakdown.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3 class="text-xl leading-6 font-medium text-gray-900 pb-4">
+            Price Breakdown
+        </h3>
+        <div v-for="(days, price) in price.breakdown" :key="price" class="py-2 border-y border-gray-500 flex justify-between">
+            <span>{{days}} days at £{{price}}</span>
+            <span>£{{days * price}}</span>
+        </div>
+        <div class="font-medium pt-4 flex justify-between">
+            <span>Total</span>
+            <span>£{{price.total}}</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        price: {
+            type: [Object, null],
+            required: true,
+        }
+    })
+</script>
