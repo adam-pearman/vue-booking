fe0fb641b59f8e7d7415f2cb9691d21e40870e40
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-08-06 10:23:46 +0100

    Update BookingsDetail.vue
    align prices across grid

--- resources/js/components/bookings/BookingsDetail.vue
+++ resources/js/components/bookings/BookingsDetail.vue
@@ -1,10 +1,11 @@
 <template>
-    <router-link :to="{name: 'booking', params: {id}}" class="bg-white overflow-hidden shadow rounded-lg">
-        <div class="px-4 py-5 sm:p-6">
-            <p class="font-medium text-lg">{{ title }}</p>
-            <p>{{ description }}</p>
-            <br>
-            <p class="font-medium">£{{ price }}</p>
+    <router-link :to="{name: 'booking', params: {id}}" class="bg-white h-full overflow-hidden shadow rounded-lg">
+        <div class="flex flex-col h-full px-4 py-5 sm:p-6">
+            <div class="h-full">
+                <p class="font-medium text-lg mb-2">{{ title }}</p>
+                <p>{{ description }}</p>
+            </div>
+            <p class="font-medium mt-6">£{{ price }}</p>
         </div>
     </router-link>
 </template>
