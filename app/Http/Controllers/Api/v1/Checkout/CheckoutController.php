fed8e926ce1a0ead46c75d10017daa974fc9c65b
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-08-06 19:33:28 +0100

    Create CheckoutController.php

--- app/Http/Controllers/Api/v1/Checkout/CheckoutController.php
+++ app/Http/Controllers/Api/v1/Checkout/CheckoutController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Api\v1\Checkout;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Checkout\CheckoutRequest;
+use App\Models\Address;
+use App\Models\Booking;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -14,8 +18,35 @@ class CheckoutController extends Controller
      * @param Request $request
      * @return Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(CheckoutRequest $request)
     {
-        //
+        $validated = $request->validated();
+        $validated = array_merge($validated, $request->validate([
+            'reservations.*' => ['required', function ($attribute, $value, $fail) {
+                $booking = Booking::findOrFail($value['booking_id']);
+
+                if (!$booking->availableFor($value['from'], $value['to'])) {
+                    $fail('This booking is not available on the given dates');
+                }
+            }],
+        ]));
+
+        $reservationsData = $validated['reservations'];
+        $addressData = $validated['customer'];
+
+        $reservations = collect($reservationsData)->map(function ($reservationData) use ($addressData) {
+            $reservation= new Reservation();
+            $reservation->from = $reservationData['from'];
+            $reservation->to = $reservationData['to'];
+            $reservation->booking_id = $reservationData['booking_id'];
+            $reservation->price = 2000;
+            $reservation->address()->associate(Address::create($addressData));
+
+            $reservation->save();
+
+            return $reservation;
+        });
+
+        return $reservations;
     }
 }
