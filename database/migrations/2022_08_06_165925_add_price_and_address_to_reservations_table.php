bdf8adeeeae84a3be66ea211825b3f904378c08c
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-08-06 18:06:07 +0100

    Create 2022_08_06_165925_add_price_and_address_to_reservations_table.php

---
+++ database/migrations/2022_08_06_165925_add_price_and_address_to_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Address;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->unsignedInteger('price');
+            $table->foreignIdFor(Address::class)->nullable()->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reservations', function (Blueprint $table) {
+            $table->dropForeignIdFor(Address::class);
+            $table->dropColumn('price');
+        });
+    }
+};
