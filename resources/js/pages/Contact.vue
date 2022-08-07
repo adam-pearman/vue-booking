9789504b1b91acee7f3ee6d1ab135841dcb4982e
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-08-07 19:43:48 +0100

    Delete Contact.vue

--- resources/js/pages/Contact.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="container mx-auto text-center">
-        <h1 class="text-4xl font-medium pb-5">Contact</h1>
-        <p>This is the contact page.</p>
-    </div>
-</template>
