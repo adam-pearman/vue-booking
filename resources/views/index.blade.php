fd1a15530005f1f8c46b4c1a1da61b1501a38040
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-08-07 14:14:23 +0100

    set app height to full

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Vue Booking</title>
 </head>
-<body class="bg-slate-200">
-    <div id="app"></div>
+<body class="h-full bg-slate-200">
+    <div id="app" class="h-full"></div>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
