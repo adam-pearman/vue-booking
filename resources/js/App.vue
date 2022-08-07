0b82ac5b021be4f7e191720e5c8c596cd3a9c846
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-08-07 14:14:52 +0100

    check for user auth on page load

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -5,4 +5,9 @@
 
 <script setup>
 import TheNav from './components/ui/TheNav.vue';
+import {useAuthStore} from "./stores/auth";
+
+const authStore = useAuthStore();
+
+authStore.getAuthUser();
 </script>
