af4968a1884209898a05b41e39cdff108c73280a
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-12-12 21:16:17 +0000

    add stripe elements cdn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="https://js.stripe.com/v3/"></script>
     <title>Vue Booking</title>
 </head>
 <body class="h-full bg-slate-200">
