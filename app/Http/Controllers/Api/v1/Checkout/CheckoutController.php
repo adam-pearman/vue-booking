c181a614475427f33e339de0fd9a90fd11ce35b0
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-12-12 21:15:39 +0000

    move address create logic to payment intent controller

--- app/Http/Controllers/Api/v1/Checkout/CheckoutController.php
+++ app/Http/Controllers/Api/v1/Checkout/CheckoutController.php
@@ -21,27 +21,17 @@ class CheckoutController extends Controller
     public function __invoke(CheckoutRequest $request)
     {
         $validated = $request->validated();
-        $validated = array_merge($validated, $request->validate([
-            'reservations.*' => ['required', function ($attribute, $value, $fail) {
-                $booking = Booking::findOrFail($value['booking_id']);
-
-                if (!$booking->availableFor($value['from'], $value['to'])) {
-                    $fail('This booking is not available on the given dates');
-                }
-            }],
-        ]));
 
         $reservationsData = $validated['reservations'];
-        $addressData = $validated['customer'];
 
-        $reservations = collect($reservationsData)->map(function ($reservationData) use ($addressData) {
+        $reservations = collect($reservationsData)->map(function ($reservationData) use ($validated) {
             $booking = Booking::findOrFail($reservationData['booking_id']);
             $reservation= new Reservation();
             $reservation->from = $reservationData['from'];
             $reservation->to = $reservationData['to'];
             $reservation->booking()->associate($booking);
             $reservation->price = $booking->priceFor($reservation->from, $reservation->to)['total'];
-            $reservation->address()->associate(Address::create($addressData));
+            $reservation->address()->associate($validated['address_id']);
 
             $reservation->save();
 
--- app/Http/Requests/Checkout/CheckoutRequest.php
+++ app/Http/Requests/Checkout/CheckoutRequest.php
@@ -29,14 +29,7 @@ class CheckoutRequest extends FormRequest
             'reservations.*.booking_id' => 'required|exists:bookings,id',
             'reservations.*.from' => 'required|date|after_or_equal:today',
             'reservations.*.to' => 'required|date|after_or_equal:bookings.*.from',
-            'customer.first_name' => 'required|min:2',
-            'customer.last_name' => 'required|min:2',
-            'customer.email' => 'required|email',
-            'customer.street' => 'required|min:5',
-            'customer.city' => 'required|min:3',
-            'customer.country' => 'required|min:2',
-            'customer.county' => 'required|min:5',
-            'customer.postcode' => 'required|min:5|max:7',
+            'address_id' => 'required|numeric|exists:addresses,id',
         ];
     }
 }
